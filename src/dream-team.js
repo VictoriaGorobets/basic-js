d7f08999a0c296aa71073405898f90bf63889c0f
VictoriaGorobets <vickyhome1996@gmail.com>
2023-12-24 19:10:57 +0200

    feat: add new solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  const validNames = members
+      .filter(member => typeof member === 'string')
+      .map(name => name.trim());
+  const dreamTeamName = validNames
+      .map(name => name[0].toUpperCase())
+      .sort()
+      .join('');
+  return dreamTeamName;
 }
 
 module.exports = {
